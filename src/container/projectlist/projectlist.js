44b591f15ad093313d6b38cb31097a6a79c58201
ashraffares <ashraffares090@gmail.com>
2021-10-14 11:51:58 +0200

    Add carousel to view projects

--- src/container/projectlist/projectlist.js
+++ src/container/projectlist/projectlist.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
+import Carousel from 'react-elastic-carousel';
 import list from '../../list';
 import Project from '../project';
 import styles from './projectlist.module.css';
 
 const Projectlist = () => {
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 1, itemsToScroll: 2 },
+    { width: 768, itemsToShow: 2 },
+    { width: 1200, itemsToShow: 3 },
+  ];
   const [projects] = useState(list);
   return (
     <main className={styles.main}>
-      {
-          projects.map((proj) => <Project key={proj.id} project={proj} />)
+      <Carousel
+        breakPoints={breakPoints}
+      >
+        {
+            projects.map((proj) => <Project key={proj.id} project={proj} />)
         }
+      </Carousel>
     </main>
   );
 };
