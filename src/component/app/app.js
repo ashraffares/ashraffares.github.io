1c2828f0784ac83e7f6e9486caab3f8091ae359f
ashraffares <ashraffares090@gmail.com>
2021-10-14 09:44:44 +0200

    chage back to pervious commit

--- src/component/app/app.js
+++ src/component/app/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 import Main from '../main';
 import Projectlist from '../../container/projectlist';
+import Rightnav from '../rightnav';
 import Topnav from '../topnav';
 import styles from './app.module.css';
 
@@ -9,9 +10,10 @@ function App() {
   return (
     <main className={styles.app}>
       <Topnav />
+      <Rightnav />
       <Switch>
         <Route path="/" component={Main} exact />
-        <Route path="/projects" component={Projectlist} exact />
+        <Route path="/projects" component={Projectlist} />
         <Redirect to="/" />
       </Switch>
     </main>
--- src/container/projectlist/projectlist.js
+++ src/container/projectlist/projectlist.js
@@ -7,13 +7,9 @@ const Projectlist = () => {
   const [projects] = useState(list);
   return (
     <main className={styles.main}>
-      <div className={styles.div}> . . . . . . . . . . . . . . . . . . . . .</div>
-      <h1 className={styles.h1}>Projects</h1>
-      <div className={styles.projectsWrapper}>
-        {
+      {
           projects.map((proj) => <Project key={proj.id} project={proj} />)
         }
-      </div>
     </main>
   );
 };
